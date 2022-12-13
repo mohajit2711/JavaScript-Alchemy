ac2423562b5cb790b0010c5dd0c7600df5afb53c
Mohajit Paul <80643937+mohajit2711@users.noreply.github.com>
2022-12-13 07:51:36 -0800

    Convert to String
    Given two values a and b, combine them together as strings and return the result. The values may be a number, a boolean or a string.

---
+++ convtoString.js
@@ -0,0 +1,13 @@
+function combineToString(a, b) {
+    a = a.toString();
+    b = b.toString();
+    return a+b;
+        
+}
+
+let a = 2;
+let b = 3;
+
+combinetoString(a,b);
+
+module.exports = combineToString;
